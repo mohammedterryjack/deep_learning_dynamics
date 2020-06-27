de5f670356faa007e00e1bfdeff7e0fd3fa2cecb
Mohammed Terry-Jack <mohammedterryjack@gmail.com>
2020-06-27 19:06:49 +0100

    clean up code for easier visualisation

---
+++ src/weight_space.py
@@ -0,0 +1,82 @@
+############   NATIVE IMPORTS  ###########################
+from typing import List
+############ INSTALLED IMPORTS ###########################
+from sklearn.neural_network import MLPClassifier
+from numpy import zeros
+from pickle import dump
+############   LOCAL IMPORTS   ###########################
+from data_loader import DataLoader
+from neural_grid_search import NeuralGridSearch
+from data_types import Vectors, Labels
+##########################################################
+
+class WeightSpaceTrainer:
+    """Map the accuracy of a given neural network's in weightspace """
+
+    def __init__(self) -> None:
+        self.x,self.y,classes = DataLoader.load("mnist")
+        self.network = self._initialise_neural_network_for_mnist_with_weights_zeroed(
+            input_layer_size=784,
+            hidden_layer_size=2,
+            output_layer_size=10,
+            x = self.x,
+            y = self.y,
+            classes = classes
+        )
+    
+    def map_weight_space(self) -> None:
+        scores = self._get_scores_for_weight_values(
+            output_layer_size=10,
+            sample_step_size=10,
+            network=self.network,
+            x = self.x,
+            y = self.y
+        )
+        dump(scores, open("test.pkl", 'wb'))
+
+   @staticmethod
+    def _initialise_neural_network_for_mnist_with_weights_zeroed(
+        input_layer_size:int, 
+        hidden_layer_size:int, 
+        output_layer_size:10,
+        x:Vectors,
+        y:Labels,
+        classes:Labels
+    ) -> MLPClassifier:
+        network = MLPClassifier(
+            hidden_layer_sizes= (hidden_layer_size), 
+            activation = "relu", 
+            alpha=1e-4,
+            solver="sgd", 
+            learning_rate_init=.1
+        )
+        network.partial_fit(x, y, classes)
+        network.coefs_[0][:][:] = zeros(
+            shape=(input_layer_size,hidden_layer_size)
+        )
+        network.coefs_[1][:][:] = zeros(
+            shape=(hidden_layer_size,output_layer_size)
+        )
+        return network
+
+    @staticmethod
+    def _get_scores_for_weight_values(
+        output_layer_size:int,
+        sample_step_size:int,
+        network:MLPClassifier,
+        x:Vectors,
+        y:Labels
+    ) -> Dict[Vectors,List[float]]:
+        data = {
+            "weights":[],
+            "scores":[]
+        }
+        for weights_1 in NeuralGridSearch.binary_vector_range(0,output_layer_size,sample_step_size):
+            for weights_2 in NeuralGridSearch.binary_vector_range(0,output_layer_size,sample_step_size):
+                network.coefs_[1][:] = [
+                    weights_1,
+                    weights_2
+                ]
+                data["weights"].append(weights_1+weights_2)
+                data["scores"].append(network.score(x,y))
+        return data 
---
+++ src/weight_space_visualiser.py
@@ -0,0 +1,83 @@
+############   NATIVE IMPORTS  ###########################
+from typing import List
+############ INSTALLED IMPORTS ###########################
+from numpy import zeros
+from seaborn import heatmap
+from matplotlib.pyplot import show
+from pandas import read_pickle
+############   LOCAL IMPORTS   ###########################
+from projection_models.projection_method import ProjectionMethod
+##########################################################
+#TODO: (find corresponding weight for that coordinate and test network)
+#TODO: work out which weights produced the better scores from the heatmap (i.e. convert their coordinates back to weights) and see if there is anything in common with them
+#TODO: if initialised in good areas of the weight space - and see how this affects training
+
+class WeightSpaceVisualiser:
+    def __init__(self, projector:ProjectionMethod) -> None:
+        self.data = read_pickle("test.pkl")
+        self.trained_projector = projector(
+            training_vectors=self.data["weights"], 
+            save_model=False
+        )
+
+    def visualise_weight_space(self) -> None:
+        coordinates = self.trained_projector.reduce_dimensions(
+            vectors= self.data["weights"]
+        )
+        x_coordinates,y_coordinates = list(zip(*coordinates))
+        lower_bound_float = min(
+            min(x_coordinates),
+            min(y_coordinates)
+        )
+        upper_bound_float = max(
+            max(x_coordinates),
+            max(y_coordinates)
+        )
+        scale = 10 / upper_bound_float
+        float_to_int = lambda number: int((number + abs(lower_bound_float))*scale)
+        lower_bound_int = float_to_int(lower_bound_float)
+        upper_bound_int = float_to_int(upper_bound_float)
+        print(
+            f"""
+            scale = {scale}
+            lower_bound_float = {lower_bound_float}
+            upper_bound_float = {upper_bound_float}
+            lower_bound_int = {lower_bound_int}
+            upper_bound_int = {upper_bound_int}
+            """
+        )
+        self._show_heatmap(
+            size = (upper_bound_int - lower_bound_int) + 1,
+            x_coordinates_as_ints = map(float_to_int,x_coordinates),
+            y_coordinates_as_ints = map(float_to_int,y_coordinates),
+            scores = self.data["scores"]
+        )
+        
+    @staticmethod
+    def _show_heatmap(
+        size:int,
+        x_coordinates_as_ints:List[int],
+        y_coordinates_as_ints:List[int],
+        scores:List[float]
+    ) -> None:
+        matrix = zeros(shape=(size,size))
+
+        for x,y,score in zip(
+            x_coordinates_as_ints,
+            y_coordinates_as_ints,
+            scores
+        ):
+            matrix[x][y] = max(matrix[x][y],score)
+
+        heatmap(data=matrix)
+        show()
+
+
+
+# from projection_models.principal_component_analysis import PrincipalComponentAnalysis
+# from projection_models.autoencoder import AutoEncoder
+# from projection_models.binary_encoder import BinaryEncoder
+
+# WeightSpaceVisualiser(
+#     projector = PrincipalComponentAnalysis,
+# ).visualise_weight_space()
