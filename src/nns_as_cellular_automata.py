3e30146e9f31347ca19f0b1632f693009e3f3bbf
Mohammed Terry-Jack <mohammedterryjack@gmail.com>
2020-06-30 10:38:17 +0100

    todos

---
+++ src/nns_as_cellular_automata.py
@@ -0,0 +1,5 @@
+#TODO: model neural network as 1D CA
+#TODO: replicate a chaotic rule / chaotic behaviour
+#TODO: plot deep nn as 1dca over time, then each iteration of the weights changing as a new 1dca over time
+#TODO: compare nn with activation functions that cause periodic or chaotic behaviour - does it allow better learning (i.e. learns to control chaos by changing weights and thus neighbours)
+#TODO: new learning paradigm? modifying activation functions rather than weights? (i.e keep all weights 1, but modify the complexity / rules of activation function)
