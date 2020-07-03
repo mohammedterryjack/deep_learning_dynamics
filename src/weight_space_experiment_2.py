c70f39efa39d7dee5030add9ba94ac12944468f4
Mohammed Terry-Jack <mohammedterryjack@gmail.com>
2020-07-03 01:35:29 +0100

    new pics

--- src/weight_space_experiment_2.py
+++ src/weight_space_experiment_2.py
@@ -14,20 +14,16 @@ from projection_models.self_organising_map import SelfOrganisingMap
 parser = ArgumentParser()
 parser.add_argument("--projection", type=str, choices=("PrincipalComponentAnalysis","SelfOrganisingMap","AutoEncoder","BinaryEncoder"), default="AutoEncoder", help="reduce dimension of network parameters for plotting")
 parser.add_argument("--iterations", type=int, choices=range(1,100), default=10, help="training iterations")
-parser.add_argument("--initialistion", type=str, choices=("min","max"), default="max", help="initialisations at best or worst places in weight space")
+parser.add_argument("--initialisation", type=str, choices=("min","max"), default="max", help="initialisations at best (max) or worst (min) places in weight space")
 args = parser.parse_args()
 
-#TODO: initialise from several good weight spaces and plot learning
-#TODO: initialise from several bad weight spaces and plot learning
 #TODO: plot training of both on same plot (average)
-#TODO: train projectors on data from other experiment as it has more range of values 
-#TODO: if initialised in good areas of the weight space (determined by other experiment) - and see how this affects training
 
 trainer = InitialisingWeightsTrainer(
     projector = eval(args.projection),
     projector_type = args.projection 
 )
-data = trainer.learn(training_iterations=args.iterations, score_selector=eval(args.initialistion))
+data = trainer.learn(training_iterations=args.iterations, score_selector=eval(args.initialisation))
 Visualiser.plot_coordinates(data)
 loaded_data = read_pickle(f"../data/weight_space_experiment/sample_size_every_10.pkl")
 coordinates = trainer.trained_projector.reduce_dimensions(
