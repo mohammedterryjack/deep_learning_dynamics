5e9edcb9ca5ce328558328044a0a24a6894bf44a
Mohammed Terry-Jack <mohammedterryjack@gmail.com>
2020-07-04 20:13:18 +0100

    new command line arg

--- src/weight_space_experiment_2.py
+++ src/weight_space_experiment_2.py
@@ -14,10 +14,9 @@ from projection_models.self_organising_map import SelfOrganisingMap
 parser = ArgumentParser()
 parser.add_argument("--projection", type=str, choices=("PrincipalComponentAnalysis","SelfOrganisingMap","AutoEncoder","BinaryEncoder"), default="AutoEncoder", help="reduce dimension of network parameters for plotting")
 parser.add_argument("--iterations", type=int, choices=range(1,100), default=10, help="training iterations")
+parser.add_argument("--resolution", type=int, choices=range(10,100), default=30, help="dimension of heatmap")
 parser.add_argument("--initialisation", type=str, choices=("min","max"), default="max", help="initialisations at best (max) or worst (min) places in weight space")
 args = parser.parse_args()
-
-#TODO: plot training of both on same plot (average)
 
 trainer = InitialisingWeightsTrainer(
     projector = eval(args.projection),
@@ -34,5 +33,5 @@ WeightSpaceVisualiser._visualise_weight_space(
     x_coordinates=list(x_coordinates) + data["x coordinate"].to_list(),
     y_coordinates=list(y_coordinates) + data["y coordinate"].to_list(),
     scores=loaded_data["scores"] + data["score"].to_list(),
-    resolution=30
+    resolution=args.resolution
 )
